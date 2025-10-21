f749eb0ce784883b5437f68363447aa4898bcaa1
entbappy <entbappy73@gmail.com>
2025-10-21 22:51:51 +0600

    day 2 added

--- Day 2/1.reading_image.py
+++ Day 2/1.reading_image.py
@@ -2,6 +2,36 @@ import cv2
 
 ## Reading images
 
-img = cv2.imread("resources/lena.png")
+# img = cv2.imread("resources/lena.png")
+# print(img)  # prints the dimensions of the image (height, width, channels)
+# cv2.imshow("Output", img)
+# cv2.waitKey(0)
 
-print(img)
+
+## Reading videos
+
+# cap = cv2.VideoCapture("resources/elon.mp4")
+
+# while True:
+#    success, img = cap.read()
+#    cv2.imshow("Output", img)
+
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#       break
+
+
+
+## Reading from webcam
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640)  # Width
+cap.set(4, 480)  # Height
+
+
+while True:
+   success, img = cap.read()
+   cv2.imshow("Output", img)
+
+   if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Computer-Vision
+
+```
+conda create -n cvdev python=3.11 -y
+
+```
+
+```
+conda activate cvdev
+```
+
+```
+pip install -r requirements.txt
+```
